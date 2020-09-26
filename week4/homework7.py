b693f0ad0ff1d27f540fdad59052633322800721
qibo <jonathan.q.bo@gmail.com>
2020-09-26 19:31:15 -0300

    provide more version of can_i_do_it

--- week4/homework7.py
+++ week4/homework7.py
@@ -1,29 +1,39 @@
-# version 1
-def can_i_do_it(learn_hard, practice_hard):
+def can_i_do_it_v1(learn_hard, practice_hard):
+    if learn_hard == True and practice_hard == True:
+        return True
+    else:
+        return False
+
+
+def can_i_do_it_v2(learn_hard, practice_hard):
+    if learn_hard is True and practice_hard is True:
+        return True
+    else:
+        return False
+
+
+def can_i_do_it_v3(learn_hard, practice_hard):
     if learn_hard and practice_hard:
         return True
     else:
         return False
 
 
-learn_hard = True
-practice_hard = True
-print('if i learn hard', learn_hard, 'and practice hard', practice_hard, ', then I can do it', can_i_do_it(learn_hard, practice_hard))
-
-learn_hard = True
-practice_hard = False
-print('if i learn hard', learn_hard, 'and practice hard', practice_hard, ', then I can do it', can_i_do_it(learn_hard, practice_hard))
-
-
-# version 2
-def can_i_do_it_v2(learn_hard, practice_hard):
+def can_i_do_it_v4(learn_hard, practice_hard):
     return learn_hard and practice_hard
 
 
 learn_hard = True
 practice_hard = True
+print('if i learn hard', learn_hard, 'and practice hard', practice_hard, ', then I can do it', can_i_do_it_v1(learn_hard, practice_hard))
 print('if i learn hard', learn_hard, 'and practice hard', practice_hard, ', then I can do it', can_i_do_it_v2(learn_hard, practice_hard))
+print('if i learn hard', learn_hard, 'and practice hard', practice_hard, ', then I can do it', can_i_do_it_v3(learn_hard, practice_hard))
+print('if i learn hard', learn_hard, 'and practice hard', practice_hard, ', then I can do it', can_i_do_it_v4(learn_hard, practice_hard))
 
 learn_hard = True
 practice_hard = False
+print('if i learn hard', learn_hard, 'and practice hard', practice_hard, ', then I can do it', can_i_do_it_v1(learn_hard, practice_hard))
 print('if i learn hard', learn_hard, 'and practice hard', practice_hard, ', then I can do it', can_i_do_it_v2(learn_hard, practice_hard))
+print('if i learn hard', learn_hard, 'and practice hard', practice_hard, ', then I can do it', can_i_do_it_v3(learn_hard, practice_hard))
+print('if i learn hard', learn_hard, 'and practice hard', practice_hard, ', then I can do it', can_i_do_it_v4(learn_hard, practice_hard))
+
